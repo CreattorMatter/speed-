f4982e09b6316d0e84d62deb3e663bf4ebdb057c
CreattorMatter <juliosoriadiaz@gmail.com>
2025-09-07 17:05:32 -0300

    Clean up Azure deployment files and update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,12 +3,6 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_ANON_KEY: string
-  readonly VITE_SUPABASE_SERVICE_KEY: string
-  readonly VITE_OPENAI_API_KEY: string
-  readonly VITE_AI_ANALYSIS_ENABLED: string
-  readonly VITE_AI_REALTIME_ANALYSIS: string
-  readonly VITE_AI_ANALYSIS_DEBOUNCE_MS: string
-  readonly VITE_MAPBOX_ACCESS_TOKEN: string
 }
 
 interface ImportMeta {
